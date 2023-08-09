1ec923eb37f41ebfc63c72d241bcb6a5f9952269
workkwaw <86957130+workkwaw@users.noreply.github.com>
2023-08-09 11:25:12 +0000

    update deps

--- mix.exs
+++ mix.exs
@@ -22,9 +22,6 @@ defmodule FoodBot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:gen_tcp_accept_and_close, "~> 0.1.0"},
       {:ex_gram, "~> 0.40.0"},
       {:tesla, "~> 1.2"},
       {:hackney, "~> 1.12"},
